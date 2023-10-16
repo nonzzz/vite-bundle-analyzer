b897733b85f85d18f4b214805c0ad56bec197bc0
kanno <812137533@qq.com>
2023-10-16 12:02:57 +0800

    feat: add test suite (#2)

---
+++ __tests__/fixtures/normal/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+function App() {
+  return <div>111</div>
+}
+
+
+ReactDOM.createRoot(document.querySelector('#app')!).render(<React.StrictMode><App /></React.StrictMode>)
