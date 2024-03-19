552998b55adffa9aac66632327d5ac6168d96687
kanno <812137533@qq.com>
2024-03-20 03:00:01 +0800

    chore: remove sirv

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,49 +2,67 @@ import http from 'http'
 import fs from 'fs'
 import type { AddressInfo } from 'net'
 import path from 'path'
-import sirv from 'sirv'
 import { clientPath, injectHTMLTag } from './shared'
 import type { Foam } from './interface'
 import type { RenderOptions } from './render'
 
-const assets = sirv(clientPath, { dotfiles: true })
+const mimeTypes: Record<string, string> = {
+  '.js': 'application/javascript',
+  '.css': 'text/css'
+}
+
+function createStaticMiddleware(options: RenderOptions, foamModule: Foam[]) {
+  const cache: Map<string, { data: Buffer | string, mimeType: string }> = new Map()
+  
+  return function staticMiddleware(req: http.IncomingMessage, res: http.ServerResponse) {
+    const filePath = path.join(clientPath, req.url!)
+    if (cache.has(filePath)) {
+      const { data, mimeType } = cache.get(filePath)!
+      res.writeHead(200, { 'Content-Type': mimeType })
+      res.end(data)
+      return
+    }
+    if (req.url === '/') {
+      res.setHeader('Content-Type', 'text/html; charset=utf8;')
+      let html = fs.readFileSync(path.join(clientPath, 'index.html'), 'utf8')
+      html = html.replace(/<title>(.*?)<\/title>/, `<title>${options.title}</title>`)
+      html = injectHTMLTag({
+        html,
+        injectTo: 'body',
+        descriptors: [`<script>
+          window.defaultSizes = ${JSON.stringify(options.mode)};\n
+          window.foamModule = ${JSON.stringify(foamModule)};\n
+          </script>`]
+      })
+      res.end(html)
+      cache.set(filePath, { data: html, mimeType: 'text/html; charset=utf8;' })
+      return
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 404
+        res.end('Not Found')
+      } else {
+        const ext = path.extname(filePath)
+        const mimeType = mimeTypes[ext] || 'application/octet-stream'
+        res.writeHead(200, { 'Content-Type': mimeType })
+        res.end(data)
+        cache.set(filePath, { data, mimeType })
+      }
+    })
+  }
+}
 
 export function createServer(port = 0) {
   const server = http.createServer()
-  server.on('request', (req, res) => {
-    assets(req, res, () => {
-      res.statusCode = 404
-      res.end('File not found')
-    })
-  })
-
+ 
   server.listen(port, () => {
     console.log(`server run on http://localhost:${(server.address() as AddressInfo).port}`)
   })
 
   const setup = (foamModule: Foam[], options: RenderOptions) => {
-    server.on('listening', () => {
-      const previousListerner = server.listeners('request')
-      server.removeAllListeners('request')
-      server.on('request', (req, res) => {
-        if (req.url === '/') {
-          res.setHeader('Content-Type', 'text/html; charset=utf8;')
-          let html = fs.readFileSync(path.join(clientPath, 'index.html'), 'utf8')
-          html = html
-            .replace(/<title>(.*?)<\/title>/, `<title>${options.title}</title>`)
-          html = injectHTMLTag({ 
-            html,
-            injectTo: 'body',
-            descriptors: [`<script>
-              window.defaultSizes = ${JSON.stringify(options.mode)};\n
-              window.foamModule = ${JSON.stringify(foamModule)};\n
-              </script>`] })
-          res.end(html)
-        } else {
-          previousListerner.map(listen => listen.call(server, req, res))
-        }
-      })
-    })
+    server.on('request', createStaticMiddleware(options, foamModule))
   }
 
   return {
