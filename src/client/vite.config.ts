1805e4a99983f99727e1f4d5a5ac3852e1466ce6
kanno <812137533@qq.com>
2024-05-09 09:34:45 +0800

    chore: switch swc minify

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -2,11 +2,17 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { stylex } from 'vite-plugin-stylex-dev'
+import { minify as _minify } from 'rollup-plugin-swc3'
 import Icons from 'unplugin-icons/vite'
-import type { UserConfig } from 'vite'
+import type { Plugin, UserConfig } from 'vite'
 
 // Because esbuild can handle esm and cjs syntax
 // so we using cjs require to import data.json
+// minify should work after vite's internal build analyzer plugin.
+function minify(): Plugin {
+  const { name, renderChunk } = _minify({ mangle: true, module: true, compress: true })
+  return { name, renderChunk: { order: 'post', handler: renderChunk as any } }
+}
 
 export default defineConfig(({ mode }) => {
   const base = <UserConfig>{
@@ -18,10 +24,11 @@ export default defineConfig(({ mode }) => {
         { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' }
       ]
     },
-    plugins: [react(), stylex({ enableStylexExtend: true }), Icons({ compiler: 'jsx', jsx: 'react' })],
+    plugins: [react(), stylex({ enableStylexExtend: true }), Icons({ compiler: 'jsx', jsx: 'react' }), minify()],
     build: {
       outDir: path.join(process.cwd(), 'dist', 'client'),
-      minify: true,
+      minify: false,
+      cssMinify: 'lightningcss',
       emptyOutDir: true
     },
     base: './'
