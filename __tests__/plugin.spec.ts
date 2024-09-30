72565b066e258efb3bed7c7abda159b2439e3892
kanno <812137533@qq.com>
2024-09-30 18:04:03 +0800

    chore: switch vitest

--- __tests__/plugin.spec.ts
+++ __tests__/plugin.spec.ts
@@ -1,8 +1,9 @@
 import path from 'path'
 import fsp from 'fs/promises'
 import fs from 'fs'
-import test from 'ava'
-import { type Logger, build } from 'vite'
+import { afterAll, describe, expect, it } from 'vitest'
+import type { Logger } from 'vite'
+import { build } from 'vite'
 import react from '@vitejs/plugin-react'
 import { analyzer } from '../dist'
 import type { AnalyzerPluginOptions } from '../src/server'
@@ -60,34 +61,35 @@ async function createBuildServer(
   return outDir
 }
 
-test.after(async () => {
-  await fsp.rm(bundlePath, { recursive: true })
-})
+describe('plugin', () => {
+  afterAll(() => fsp.rm(bundlePath, { recursive: true }))
+  describe('logger', () => {
+    it('log summary', async () => {
+      const logger = createLogger()
+      await createBuildServer('normal', { analyzerMode: 'json', summary: true }, logger)
+      const actual = logger.messages.find(log => log.message.includes('chunks of'))
+      expect(!!actual).toBeTruthy()
+    })
 
-test.serial('generator JSON', async (t) => {
-  const id = await createBuildServer('normal', { analyzerMode: 'json' })
-  const statsPath = path.join(id, 'stats.json')
-  await sleep(3000)
-  t.is(fs.existsSync(statsPath), true)
-})
-
-test.serial('generator Static Page', async (t) => {
-  const id = await createBuildServer('normal', { analyzerMode: 'static' })
-  const analyzerPath = path.join(id, 'stats.html')
-  await sleep(3000)
-  t.is(fs.existsSync(analyzerPath), true)
-})
-
-test.serial('log summary', async (t) => {
-  const logger = createLogger()
-  await createBuildServer('normal', { analyzerMode: 'json', summary: true }, logger)
-  const actual = logger.messages.find(log => log.message.includes('chunks of'))
-  t.truthy(!!actual)
-})
-
-test.serial('not log summary', async (t) => {
-  const logger = createLogger()
-  await createBuildServer('normal', { analyzerMode: 'json', summary: false }, logger)
-  const actual = logger.messages.some(log => log.message.includes('chunks of'))
-  t.is(actual, false)
+    it('not log summary', async () => {
+      const logger = createLogger()
+      await createBuildServer('normal', { analyzerMode: 'json', summary: false }, logger)
+      const actual = logger.messages.some(log => log.message.includes('chunks of'))
+      expect(actual).toBe(false)
+    })
+  })
+  describe('generator', () => {
+    it('json', async () => {
+      const id = await createBuildServer('normal', { analyzerMode: 'json' })
+      const statsPath = path.join(id, 'stats.json')
+      await sleep(3000)
+      expect(fs.existsSync(statsPath)).toBe(true)
+    })
+    it('static', async () => {
+      const id = await createBuildServer('normal', { analyzerMode: 'static', openAnalyzer: false })
+      const analyzerPath = path.join(id, 'stats.html')
+      await sleep(3000)
+      expect(fs.existsSync(analyzerPath)).toBe(true)
+    })
+  })
 })
--- __tests__/shared.spec.ts
+++ __tests__/shared.spec.ts
@@ -1,11 +1,11 @@
-import test from 'ava'
+import { describe, expect } from 'vitest'
 import { convertBytes } from '../src/shared'
 
-test('convert bytes', async (t) => {
-  t.is(convertBytes(0), '0 Bytes')
-  t.is(convertBytes(Math.pow(1024, 1)), '1.00 KB')
-  t.is(convertBytes(Math.pow(1024, 2)), '1.00 MB')
-  t.is(convertBytes(Math.pow(1024, 3)), '1.00 GB')
+describe('convert bytes', () => {
+  expect(convertBytes(0), '0 Bytes')
+  expect(convertBytes(Math.pow(1024, 1)), '1.00 KB')
+  expect(convertBytes(Math.pow(1024, 2)), '1.00 MB')
+  expect(convertBytes(Math.pow(1024, 3)), '1.00 GB')
   // TODO: seems like incorrect
-  t.is(convertBytes(Math.pow(1024, 4)), '4294967296.00 TB')
+  expect(convertBytes(Math.pow(1024, 4)), '4294967296.00 TB')
 })
