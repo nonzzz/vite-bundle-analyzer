bb4235953416f1bfa4ba2ba5032506704be2793a
SukkaW <isukkaw@gmail.com>
2024-10-05 01:34:02 +0800

    refactor: adopt foxact more

--- src/client/composables/use-dom-observer/use-dom-observer.ts
+++ src/client/composables/use-dom-observer/use-dom-observer.ts
@@ -1,23 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect } from 'foxact/use-abortable-effect'
 import type { MutableRefObject } from 'react'
 
 export function useDOMObserver(
   ref: MutableRefObject<HTMLElement | null> | undefined,
-  callback: MutationCallback = () => {}
+  callback: MutationCallback = () => { }
 ) {
   const config = { attributes: false, childList: true, subtree: true }
 
-  useEffect(() => {
+  useEffect((signal) => {
     if (!ref || !ref.current) return
-    let unmount = false
     const done: MutationCallback = (...params) => {
-      if (unmount) return
+      if (signal.aborted) return
       callback(...params)
     }
     const observer = new MutationObserver(done)
     observer.observe(ref.current, config)
     return () => {
-      unmount = true
       observer.disconnect()
     }
   }, [ref])
