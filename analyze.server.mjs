f297598a25470f9f152325dc7938f5e3f9457dbc
kanno <812137533@qq.com>
2026-02-02 08:56:45 +0800

    chore: fix ci

--- analyze.server.mjs
+++ analyze.server.mjs
@@ -1,11 +1,34 @@
 //  I won't consider rolldown dts plugin, isolation declaration isn't match for me.
 // I have no time to change my code to fit with isolation declaration.
 // I'll create a simple dts gen to my current need.
+import fs from 'fs'
 import { builtinModules } from 'module'
+import path from 'path'
 import { defineConfig } from 'rolldown'
 import { minify } from 'rollup-plugin-swc3'
 import { analyzer, unstableRolldownAdapter } from './dist/index.mjs'
 const external = [...builtinModules, 'vite']
+
+const defaultWD = process.cwd()
+
+const WASM_BINARY_PATH = path.join(defaultWD, 'zig-out', 'scan.wasm')
+
+const WASM_BINARY_B64 = fs.readFileSync(WASM_BINARY_PATH, 'base64')
+
+function virtualWASM() {
+  return {
+    name: 'wasm',
+    transform(code) {
+      if (code.includes('declare const b64: string')) {
+        code = code.replace('declare const b64: string', `const b64 = \`${WASM_BINARY_B64}\``)
+        return {
+          code,
+          map: { mappings: '' }
+        }
+      }
+    }
+  }
+}
 
 export default defineConfig({
   input: {
@@ -19,6 +42,7 @@ export default defineConfig({
     { dir: 'analysis', format: 'cjs', exports: 'named', entryFileNames: '[name].js' }
   ],
   plugins: [
+    virtualWASM(),
     {
       name: 'resolve-template',
       resolveId: {
